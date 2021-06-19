0e7c10e60277b4a97cada3cb659eb7e40e379779
AdamWang518 <dodoga518@gmail.com>
2021-06-19 21:37:17 +0800

    Add

--- hospitaladd.php
+++ hospitaladd.php
@@ -1,11 +1,34 @@
 <?php
 require "DataBase.php";
 $db = new DataBase();
+$_POST = json_decode(file_get_contents('php://input'), true);
 if (isset($_POST['name']) && isset($_POST['city'])&& isset($_POST['town'])&& isset($_POST['road'])) {
     if ($db->dbConnect()) {
         if ($db->HA("hospital_opendata", $_POST['name'], $_POST['city'], $_POST['town'], $_POST['road'])) {
-            echo "Add Success";
-        } else echo "Add Failed";
-    } else echo "Error: Database connection";
-} else echo "All fields are required";
+            $json = Array(
+                "HttpCode" => 200,
+                "message" => "success"
+            );   
+            echo json_encode($json); 
+        } else{
+            $json = Array(
+                "HttpCode" => 500,
+                "message" => "error insert db"
+            );   
+            echo json_encode($json); 
+        }
+    } else {
+        $json = Array(
+            "HttpCode" => 500,
+            "message" => "error connection db"
+        );   
+        echo json_encode($json); 
+    }
+} else {
+    $json = Array(
+        "HttpCode" => 500,
+        "message" => "error all fields  required"
+    );   
+    echo json_encode($json); 
+}
 ?>
--- location.php
+++ location.php
@@ -1,11 +1,34 @@
 <?php
 require "DataBase.php";
 $db = new DataBase();
+$_POST = json_decode(file_get_contents('php://input'), true);
 if (isset($_POST['city']) && isset($_POST['town'])&&isset($_POST['road'])&&isset($_POST['latitude'])&&isset($_POST['longitude'])) {
     if ($db->dbConnect()) {
         if ($db->locaion("location", $_POST['city'], $_POST['town'], $_POST['road'], $_POST['latitude'], $_POST['longitude'])) {
-            echo "Add Location Success";
-        } else echo "Add Location Failed";
-    } else echo "Error: Database connection";
-} else echo "All fields are required";
+            $json = Array(
+                "HttpCode" => 200,
+                "message" => "success"
+            );   
+            echo json_encode($json); 
+        } else{
+            $json = Array(
+                "HttpCode" => 500,
+                "message" => "error insert db"
+            );   
+            echo json_encode($json); 
+        }
+    } else {
+        $json = Array(
+            "HttpCode" => 500,
+            "message" => "error connection db"
+        );   
+        echo json_encode($json); 
+    }
+} else {
+    $json = Array(
+        "HttpCode" => 500,
+        "message" => "error all fields  required"
+    );   
+    echo json_encode($json); 
+}
 ?>
